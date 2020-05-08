246be2c95e09e6424a3d700dadb246a8203689e6
Monnier Milo <milo.monnier@irstea.fr>
2020-05-09 00:12:19 +0200

    update rewiring tools

--- R/rewiring_tools.R
+++ R/rewiring_tools.R
@@ -170,3 +170,40 @@ lengthenShortEdge = function(g)
 }
 
 
+#' Adjust the average degree of a graph
+#'
+#' During a rewiring process, several operations such as nodes deletion
+#' can make change the average degree of a graph. This function aims to
+#' re-adjust the number of edges to a given level by adding or deleting edges
+#' randomly.
+#'
+#' @param g an igraph object.
+#' @param avg.degree numeric; the expected average degree.
+#' @param ... other arguments passed to \code{\link{addRandomEdges}} to
+#' constrain the add of edges. For example, if we want the edges to be added
+#' to long supply chains, we pass \code{v2connect=V(g)[type=="I"]}
+#'
+#' @return an igraph object.
+#' @export
+#'
+#' @examples
+#' library(igraph)
+#' g = generateSupplyNet()
+#' plot(g)
+#' ecount(g) / vcount(g)
+#' g = adjustAvgDegree(g, avg.degree=3)
+#' ecount(g) / vcount(g)
+#' plot(g)
+#'
+adjustAvgDegree = function(g,
+                           avg.degree = 1.470968,
+                           ...)
+{
+  expE = round(vcount(g) * avg.degree)
+  while (length(E(g)) < expE)
+    g = addRandomEdges(g, ...)
+  while (length(E(g)) > expE)
+    g = deleteRandomEdges(g, supply.cuts=FALSE, bridges=FALSE)
+  return(g)
+}
+
